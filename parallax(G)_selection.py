ff540a1d9c11e7288efca2cc4690a825d045b3a3
olgaborodina <43516628+olgaborodina@users.noreply.github.com>
2018-10-01 10:59:41 +0500

    Added complex selection
    Depending on star's magnitude (due to Gaia DR2 features) selection is different

--- parallax(G)_selection.py
+++ parallax(G)_selection.py
@@ -1,7 +1,7 @@
 #Variables
 NUMSTRINGS=56230 #number of strings in input file. Should be replaced in the future
 j=0 #variable for reading file. Should be replaced in the future
-dPar=1.5 # interval for parallax-selection
+dPar=0.5 # interval for parallax-selection
 # opening result-file
 try:        
     g=open('NGC2516_100m_selected.txt','w')
@@ -14,15 +14,25 @@ with open('NGC2516_100m.tsv', 'r') as f:
         line = f.readline()
         star=line.split(';')
         if (len(star)>15):
-            try:
-                if abs(float(star[8])-2.5)>dPar:                    
-                    print('too far')
-                else: 
-                    g.write(line)
-                    print(j)
-            except ValueError:
-                print('aaaaaa')
-            j=j+1
+            if (float(star[15])<18):
+                try:
+                    if abs(float(star[8])-2.5)>dPar:                    
+                        print('too far')
+                    else: 
+                        g.write(line)
+                        print(j)
+                except ValueError:
+                    print('aaaaaa')
+            else:
+                try:
+                    if abs(float(star[8])-2.5)>(dPar+(float(star[15])-18)*0.65):                    
+                        print('too far___')
+                    else: 
+                        g.write(line)
+                        print(j)
+                except ValueError:
+                    print('aaaaaa___')               
+        j=j+1
 f.close()
 g.close()
 print ('I`ve worked so much, bring me some coffee')
