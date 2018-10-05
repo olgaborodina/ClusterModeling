05f7444edc646a3e859fb74413d8d0c04b56ed70
olgaborodina <43516628+olgaborodina@users.noreply.github.com>
2018-10-06 01:30:34 +0500

    Added new version
    1) Added function to define parameters
    2) Added pandas lib
    3) Changed a way of transforming str to float
    For future: need to parser name of input file

--- all_plots.py
+++ all_plots.py
@@ -1,73 +1,44 @@
-# script makes four kinds of plots: parallax (G), proper motion (G) in two 
-# coordinates: RA and DEC, and also Color index - Magnitude Diagram
+# script makes three kinds of star selection, depending on: parallax,
+# proper motion in two coordinates: RA and DEC. 
 import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
 #Variables
 xlist = [] #array for x
 ylist = [] #array for y
 
 InFile =input("Input the name of your file")
-ch = input("Write 'p' for parallax plot, 'a' for proper motion plot in RA, 'd' for proper motion plot in DEC and 'c' for CMD")
+mode = input("Write 'p' for parallax plot, 'a' for proper motion plot in RA, 'd' for proper motion plot in DEC and 'c' for CMD")
 
+def get_parameters(mode): 
+    if (mode == 'p'):
+        return 15,8,'G, mag','parallax, mas','par_plot'
+    elif (mode == 'a'):
+        return 15,10,'G, mag','pm in RA, mas/y','pminRA_plot'
+    elif (mode == 'd'):
+        return 15,12,'G, mag','pm in DEC, mas/y','pminDEC_plot'
+    elif (mode == 'c'):
+        return 21,15,'BP-RP, mag','G, mag','CMD_plot'
+    else:
+        raise ValueError # what`s name of this Error?
 
-if (ch == 'p'):
-    Nx=15                   # Number in initial array for х
-    Ny=8                    # Number in initial array for  у
-    xl='G, mag'             # Name for x-axis
-    yl='parallax, mas'      # Name for y-axis
-    name='parallax_plot'    # Name for plot
-   
-elif (ch == 'a'):
-    Nx=15                   # Number in initial array for  х
-    Ny=10                   # Number in initial array for  у
-    xl='G, mag'             # Name for x-axis
-    yl='pm in RA, mas/y'    # Name for y-axis
-    name='pminRA_plot'      # Name for plot
-    
-elif (ch == 'd'):
-    Nx=15                   # Number in initial array for  х
-    Ny=12                   # Number in initial array for  у
-    xl='G, mag'             # Name for x-axis
-    yl='pm in DEC, mas/y'   # Name for y-axis
-    name='pminDEC_plot'     # Name for plot
-        
-    
-elif (ch == 'c'):
-    Nx=21                   # Number in initial array for  х
-    Ny=15                   # Number in initial array for  у
-    xl='BP-RP, mag'         # Name for x-axis
-    yl='G, mag'             # Name for y-axis
-    name='CMD_plot'         # Name for plot
-else:
-    print('Error "please check your input letter and try again"')
+Nx, Ny, xl, yl, name = get_parameters(mode)
 
-def get_float(st): #function for getting float from string
-    try:
-        return float(st)
-    except ValueError:
-        return None
-    
-with open(f"{InFile}", 'r') as f:  #reading file and making arrays 
-    lines = f.readlines()
-xlist = [line.split(';')[Nx] for line in lines]
-ylist = [line.split(';')[Ny] for line in lines]
-f.close()
+data = pd.read_csv(f"{InFile}", delimiter=';', header=None)
+data.rename(columns = {Nx : xl, Ny : yl}, inplace=True)
 
-x = [get_float(elem) for elem in xlist]
-y = [get_float(elem) for elem in ylist]
+data[xl] = pd.to_numeric(data[xl],errors='coerce')
+data[yl] = pd.to_numeric(data[yl],errors='coerce')
 
 # making plot
 fig, ax = plt.subplots(figsize=(16, 14)) #16:14 is szi==ize ratio 
 
-ax.scatter(x, y, s=8, facecolor='white', edgecolor='black') 
+ax.scatter(data[xl], data[yl], s=8, facecolor='white', edgecolor='black') 
 ax.grid(c='#aaaaaa', ls='--')
-if (ch != 'c'):        
+if (mode != 'c'):        
     ax.set_ylim(-15, 15)      #cutting area for plot
-if (ch == 'c'): 
+if (mode == 'c'): 
     ax.invert_yaxis() 
 plt.xlabel(f"{xl}")
 plt.ylabel(f"{yl}")
 plt.savefig(f"{name}.png", dpi=200)
-
-
