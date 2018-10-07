6e6da3cecd1638da3c089be7eacb3d25f2136fca
olgaborodina <43516628+olgaborodina@users.noreply.github.com>
2018-10-07 13:01:50 +0500

    Added "smart" name of output file
    1) Name of output file depends on input file and parameters
    2) All data in input file gets NaNs
    3) Changed names of variables
    4) Changed description

--- all_selections.py
+++ all_selections.py
@@ -1,36 +1,43 @@
-# script makes four kinds of plots: parallax (G), proper motion (G) in two 
-# coordinates: RA and DEC, and also Color index - Magnitude Diagram
+# script makes three kinds of star selection, depending on: parallax,
+# proper motion in two coordinates: RA and DEC.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-In_File =input("Input the name of your file")
-#In_File ='NGC2516_100m.tsv'
+in_file =input("Input the name of your file")
 mode= input("Write 'p' for parallax selection, 'a' for proper motion selection in RA, 'd' for proper motion selection in DEC")
 
 def get_parameters(mode): 
     if (mode == 'p'):
         return 15,8,'G, mag','parallax, mas','par',0.3,0.75
     elif (mode == 'a'):
-        return 15,10,'G, mag','pm in RA, mas/y','pminRA',3,0.3
+        return 15,10,'G, mag','pm in RA, mas/y','pminRA',1.5,0.3
     elif (mode == 'd'):
         return 15,12,'G, mag','pm in DEC, mas/y','pminDEC',3,0.3
     else:
         raise ValueError # what`s name of this Error?
 
-Nx, Ny, xl,yl, name, delt, tgA = get_parameters(mode)
-    
+Numx, Numy, x_name,y_name, name, delt, tgA = get_parameters(mode)
+
+# define the name of input file
+par_name=in_file.split('_')
+len (par_name)
+if len(par_name) <= 2:
+    out_file = f"{par_name[0]}_{par_name[1].split('.tsv')[0]}_selected_{name}_{delt}_{tgA}.txt"
+else:
+    out_file = f"{in_file.split('.txt')[0]}_{name}_{delt}_{tgA}.txt"    
+
 # reading input file  and creating data-arrays
-data = pd.read_csv(f"{In_File}", delimiter=';', header=None)
-data.rename(columns = {Nx : xl, Ny : yl}, inplace=True)
-pd.to_numeric(data[xl],errors='coerce')
-pd.to_numeric(data[yl],errors='coerce')                          
+data = pd.read_csv(in_file, delimiter=';', header=None)
+data.rename(columns = {Numx : x_name, Numy : y_name}, inplace=True)
+data = data.apply(pd.to_numeric, errors='coerce')
+
 #print (data.head())
 
 # cluster search
-data['bin'] = pd.cut(bins=np.linspace(-15, 15, 1000), x=data[yl]) #splitting interval
+data['bin'] = pd.cut(bins=np.linspace(-15, 15, 1000), x=data[y_name]) #splitting interval
 
-data['weight'] = np.exp(-data[xl])
+data['weight'] = np.exp(-data[x_name])
 dist = data.groupby('bin')[['weight']].sum()
 dist['average Gmag'] = dist.index.map(lambda x: x.right).astype(float)
 
@@ -42,21 +49,18 @@ plt.show()
 print(dist['weight'].idxmax())
 mid_value=dist['weight'].idxmax().mid #most likely value of parallax or proper motion for Cluster
 
-#print (data[xl])
-#print (data.values[100][0])
+#making an array of interval values
 x=data['G, mag']
 delta_vec = pd.Series(len(x) * [np.nan])
 delta_vec[x < 18] = delt 
 delta_vec[x >= 18] = delt + (data['G, mag'] - 18) * tgA
 
 def f(x, tgA):
-    return delta_vec
-    
+    return delta_vec    
 delta = f(data['G, mag'], tgA)
 
 #selection           
-mask = (mid_value - delta < data[yl]) & (data[yl] < mid_value + delta)
-data.loc[mask].to_csv(f"NGC2516_100m_selected_{name}_{delt}_{tgA}.txt", sep=';',header= False, index=False)
+mask = (mid_value - delta < data[y_name]) & (data[y_name] < mid_value + delta)
+data.loc[mask].to_csv(out_file, sep=';',header= False, index=False)
 
-print ('I`ve worked so much, bring me some coffee')  # you have not deserved it yet
-
+print ('I`ve worked so much, bring me some coffee')
