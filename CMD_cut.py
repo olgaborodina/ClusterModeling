88ed88093ff2ae5f2a8959a56ee004a660744501
olgaborodina <43516628+olgaborodina@users.noreply.github.com>
2018-11-01 23:10:23 +0500

    Added a posibility of manual filtering
    1) Added description
    2) After faint stars selection you can continue to drop stars manually (with Bokeh)
    3) Also added a posibility of retrieval data in case of mistake (!)

--- CMD_cut.py
+++ CMD_cut.py
@@ -1,11 +1,15 @@
-# script makes three kinds of star selection, depending on: parallax,
-# proper motion in two coordinates: RA and DEC.
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
+# script for deleting the rest of non-cluster stars by cutting
+# faint stars and also by choosing some manually
 
-in_file =input("Input the name of your file")
+import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from bokeh.plotting import figure, show, ColumnDataSource, output_file
+from bokeh.models import HoverTool
+
+#in_file =input("Input the name of your file")
+in_file='NGC2516_100m_selected_parallax_0.2_pm_inRA_1.5_pm_inDEC_1.5.txt'
 
 Numx, Numy, x_name,y_name, name= 21, 15, 'BP-RP, mag','G, mag','CMD_cutted'
 
@@ -22,24 +26,58 @@ def f(x, a, b, c, d):
 _array = data.dropna(subset=['BP-RP, mag']).loc[data.dropna(subset=['BP-RP, mag'])['G, mag'] > 14]
 popt, pcov = curve_fit(f, _array['G, mag'], _array['BP-RP, mag'])
 
-#selection
+#selection by magnitude limit
 G_limit = _array['G, mag'].loc[f(_array['G, mag'], *popt).idxmax()]           
 mask = (G_limit > data[y_name])
+data=data.loc[mask]
 
-# define the name of input file
+# rename indexes for rows
+data.index = pd.RangeIndex(len(data.index))
+
+# function for making CMD using one data
+def plot_CMD (data):
+    source = ColumnDataSource(
+            data=dict(
+                x=data[x_name],
+                y=data[y_name],
+            )
+        )
+    hover = HoverTool(
+            tooltips=[
+                ("index", "$index"),
+                ("(x,y)", "($x, $y)"),
+            ]
+        )
+    output_file('CMD.html')
+    p = figure(plot_width=600, plot_height=600, tools=[hover],x_range = (-0.5, 3.5), y_range = (19, 4))
+    p.circle('x', 'y', size=4, source=source, color = "black", alpha = 0.75)
+    return show(p)
+
+plot_CMD(data)
+
+# changing data manually! too risky!
+command = 'continue'
+
+while command == 'continue':
+    to_do, index = input("1. 'd' to delete star, 's' to stop, 'r' to return previous data 2. index of star").split(' ')
+    index = int(index)
+    if to_do == 'd':    
+        data_saved = data
+        data = data.drop(index)
+        data.index = pd.RangeIndex(len(data.index))
+        plot_CMD(data) 
+    if to_do == 'r':
+         data = data_saved          
+    if to_do == 's':
+        command = 'stop'
+        
+# define the name of input file and write there
 par_name=in_file.split('_')
 len (par_name)
 if len(par_name) <= 2:
     out_file = f"{par_name[0]}_{par_name[1].split('.tsv')[0]}_{name}_{G_limit}.txt"
 else:
     out_file = f"{in_file.split('.txt')[0]}_{name}_{G_limit}.txt"  
-    
-
-data=data.loc[mask]
-#data.loc[mask].to_csv(out_file, sep=';', header= False, index=False)
-
-
-
 data.to_csv(out_file, sep=';', header= False, index=False)
 
 print ('I`ve worked so much, bring me some coffee')
