fdff616e2328c6678d229ec58ab50b9e1a7dcdcf
olgaborodina <43516628+olgaborodina@users.noreply.github.com>
2018-11-04 22:15:18 +0500

    Added a posibility of chaging existed separation
    1) Now you can continue your separation from where you ended by using existed files (made a special func)
    2) Fixed a bug with plot (by matplotlib)
    3) Deleted some things that was slowing process

--- CMD_separation.py
+++ CMD_separation.py
@@ -1,4 +1,4 @@
-# script for separation the stars into binaries and singles
+# script for separation the stars into binaries and single stars
 
 import pandas as pd
 import numpy as np
@@ -7,20 +7,29 @@ import matplotlib.pyplot as plt
 from bokeh.plotting import figure, show, ColumnDataSource, output_file
 from bokeh.models import HoverTool
 
-#in_file =input("Input the name of your file")
-in_file='NGC2516_100m_selected_parallax_0.2_pm_inRA_1.5_pm_inDEC_1.5_CMD_cutted_18.6811.txt'
-
 Numx, Numy, x_name,y_name, name= 21, 15, 'BP-RP, mag','G, mag','CMD_separated'
 
-# reading input file  and creating data-arrays
-data = pd.read_csv(in_file, delimiter=';', header=None)
-data.rename(columns = {Numx : x_name, Numy : y_name}, inplace=True)
-data = data.apply(pd.to_numeric, errors='coerce')
+#reading inputs
+def reading(file):
+    data = pd.read_csv(file, delimiter=';', header=None)
+    data.rename(columns = {Numx : x_name, Numy : y_name}, inplace=True)
+    data = data.apply(pd.to_numeric, errors='coerce')
+    data.index = pd.RangeIndex(len(data.index))
+    return data
 
-# rename indexes for rows
-data.index = pd.RangeIndex(len(data.index))
-
-# function for making CMD using one data
+que=input("Do you want to work with a new file? 'y' or 'n'")
+if que == 'y':
+    f = input("Input the name of your file")
+    data_singles = reading(f)    
+    data_binaries = pd.DataFrame()
+if que == 'n':
+    f_s = input("Input the name of your file with single stars")
+    f_b = input("Input the name of your file with binary stars")
+    data_singles = reading(f_s)    
+    data_binaries = reading (f_b)
+    f = f_s.split('_CMD_separated_singles')[0] + f_s.split('_CMD_separated_singles')[1]
+    
+# function for making CMD 
 def plot_CMD (data):
     source = ColumnDataSource(
             data=dict(
@@ -35,27 +44,24 @@ def plot_CMD (data):
             ]
         )
     output_file('CMD.html')
-    p = figure(plot_width=600, plot_height=600, tools=[hover],x_range = (-0.5, 3.5), y_range = (19, 4))
-    p.circle('x', 'y', size=4, source=source, color = "black", alpha = 0.75)
+    p = figure(plot_width=900, plot_height=900, tools=[hover],x_range = (-0.5, 3.5), y_range = (19, 4))
+    p.circle('x', 'y', size=3, source=source, color = "black", alpha = 0.75)
     return show(p)
 
-plot_CMD(data)
+plot_CMD(data_singles)
 
 # changing data manually! too risky!
-data_binaries = pd.DataFrame()
-#data_binaries.rename(columns = {Numx : x_name, Numy : y_name}, inplace=True)
-data_singles = data
 command = 'continue'
 
 while command == 'continue':
-    to_do, index = input("1. 'b' for separation binary, 's' for stop, 'r' for returning previous data 2. index of star").split(' ')
+    to_do, index = input("1. 'b' for separation binary, 's 0' for stop, 'r' for returning previous data 2. index of star").split(' ')
     index = int(index)
     if to_do == 'b':
         data_saved_b = data_binaries
         data_saved_s = data_singles
         
         data_binaries = data_binaries.append(data_singles.loc[index])
-        data_binaries = data_binaries[data.columns]
+        data_binaries = data_binaries[data_singles.columns]
         data_singles = data_singles.drop(index)
         data_singles.index = pd.RangeIndex(len(data_singles.index))
         data_binaries.index = pd.RangeIndex(len(data_binaries.index))
@@ -71,8 +77,6 @@ while command == 'continue':
         plt.xticks(np.arange(-0.5, 4, step=0.5))
         plt.xlabel(f"{x_name}")
         plt.ylabel(f"{y_name}")
-        plt.show()
-        plt.savefig(f"{in_file.split('.txt')[0]}_{name}_plot.png", dpi=200)
         plot_CMD(data_singles)
     if to_do == 'r':
          data_singles = data_saved_s
@@ -81,7 +85,8 @@ while command == 'continue':
         command = 'stop'
         
 # output files
-data_singles.to_csv(f"{in_file.split('.txt')[0]}_{name}_singles.txt", sep=';', header= False, index=False)
-data_binaries.to_csv(f"{in_file.split('.txt')[0]}_{name}_binaries.txt"  , sep=';', header= False, index=False)
-plt.savefig(f"{in_file.split('.txt')[0]}_{name}_plot.png", dpi=200)
-print ('I`ve worked so much, bring me some coffee')
+data_singles.to_csv(f"{f.split('.txt')[0]}_{name}_singles.txt", sep=';', header= False, index=False)
+data_binaries.to_csv(f"{f.split('.txt')[0]}_{name}_binaries.txt"  , sep=';', header= False, index=False)
+plt.savefig(f"{f.split('.txt')[0]}_{name}_plot.png", dpi=200)
+
+print ('I`ve worked so much, I deserve a chocolate bar! pleeeease!')
