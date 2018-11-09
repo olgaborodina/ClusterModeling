9b7d7ce99f32f780ac543799df53bcac54f058eb
olgaborodina <43516628+olgaborodina@users.noreply.github.com>
2018-11-09 16:12:33 +0500

    Added choosing manual magnitude limitation
    You can cut your CMD by your magnitude value or by automatically chosen one

--- CMD_cut.py
+++ CMD_cut.py
@@ -8,8 +8,8 @@ from scipy.optimize import curve_fit
 from bokeh.plotting import figure, show, ColumnDataSource, output_file
 from bokeh.models import HoverTool
 
-#in_file =input("Input the name of your file")
-in_file='NGC2516_100m_selected_parallax_0.2_pm_inRA_1.5_pm_inDEC_1.5.txt'
+in_file =input("Input the name of your file")
+#in_file='NGC2516_100m_selected_parallax_0.2_pm_inRA_1.5_pm_inDEC_1.5.txt'
 
 Numx, Numy, x_name,y_name, name= 21, 15, 'BP-RP, mag','G, mag','CMD_cutted'
 
@@ -18,16 +18,23 @@ data = pd.read_csv(in_file, delimiter=';', header=None)
 data.rename(columns = {Numx : x_name, Numy : y_name}, inplace=True)
 data = data.apply(pd.to_numeric, errors='coerce')
 
-# trying to find turning spot on CMD
-def f(x, a, b, c, d):
-    return a * x * x * x + b * x * x + c * x + d
 
-#_array is a frame for making approximation of unfiltered main sequence 
-_array = data.dropna(subset=['BP-RP, mag']).loc[data.dropna(subset=['BP-RP, mag'])['G, mag'] > 14]
-popt, pcov = curve_fit(f, _array['G, mag'], _array['BP-RP, mag'])
+to_cut = input("'m' to choose a limit manually and 'a' to define a limit automatically")
 
-#selection by magnitude limit
-G_limit = _array['G, mag'].loc[f(_array['G, mag'], *popt).idxmax()]           
+if to_cut == 'm':
+    G_limit = float(input("G_limit"))
+if to_cut == 'a':
+    
+    # trying to find turning spot on CMD
+    def f(x, a, b, c, d):
+        return a * x * x * x + b * x * x + c * x + d
+
+    #_array is a frame for making approximation of unfiltered main sequence 
+    _array = data.dropna(subset=['BP-RP, mag']).loc[data.dropna(subset=['BP-RP, mag'])['G, mag'] > 14]
+    popt, pcov = curve_fit(f, _array['G, mag'], _array['BP-RP, mag'])
+
+    #selection by magnitude limit
+    G_limit = _array['G, mag'].loc[f(_array['G, mag'], *popt).idxmax()]           
 mask = (G_limit > data[y_name])
 data=data.loc[mask]
 
@@ -49,7 +56,7 @@ def plot_CMD (data):
             ]
         )
     output_file('CMD.html')
-    p = figure(plot_width=600, plot_height=600, tools=[hover],x_range = (-0.5, 3.5), y_range = (19, 4))
+    p = figure(plot_width=800, plot_height=800, tools=[hover],x_range = (-0.5, 3.5), y_range = (19, 4))
     p.circle('x', 'y', size=4, source=source, color = "black", alpha = 0.75)
     return show(p)
 
